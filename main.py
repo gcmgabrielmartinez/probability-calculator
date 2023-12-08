4dccaaa7b7abbd79f0552cb79f3d7a46ea95389c
gcmgabrielmartinez <gabrielcalvo2812@gmail.com>
2023-12-08 14:39:36 +0000

    Initial commit

--- main.py
+++ main.py
@@ -12,5 +12,11 @@ probability = prob_calculator.experiment(
     num_experiments=3000)
 print("Probability:", probability)
 
+hat = prob_calculator.Hat(blue=3,red=2,green=6)
+probability = prob_calculator.experiment(hat=hat, expected_balls={"blue":2,"green":1}, num_balls_drawn=4, num_experiments=1000)
+
+
+print("Probability:", probability)
+
 # Run unit tests automatically
 main(module='test_module', exit=False)
--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,47 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+  def __init__(self, **kwargs):
+    contents = []
+    for key, value in kwargs.items():
+      for i in range(value):
+        contents.append(key)
+    self.contents = contents
 
+  def draw(self, num_balls_drawn):
+    if num_balls_drawn >= len(self.contents):
+      return self.contents
+    else:
+      balls_drawn = []
+      self.contents
+      for i in range(num_balls_drawn):
+        balls_drawn.append(self.contents.pop(random.randint(0, len(self.contents)-1)))
+      return balls_drawn
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  num_success = 0
+  succed = 0
+  for i in range(0,num_experiments):
+    hat_copy = copy.deepcopy(hat)
+    pop_ball = hat_copy.draw(num_balls_drawn)
+    pop_ball_dict = {}
+    for ball in pop_ball:
+      try:
+        pop_ball_dict[ball] += 1
+      except:
+        pop_ball_dict.update({ball:1})
+      
+    
+    try:
+      for key, value in expected_balls.items():
+        if pop_ball_dict[key] >= value:
+          succed = 1
+        else:
+          succed = 0
+          break
+    except:
+      succed = 0
+      pass
+    num_success += succed
+
+  return num_success/num_experiments
